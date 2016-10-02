40c1421164bae81db629f05c2690f0df1ddc73a7
Mangosteen <yangshuo225@gmail.com>
2016-10-02 17:32:46 +1100

    add current test message

--- 16s2/9021/assignment2/run_all_txt.py
+++ 16s2/9021/assignment2/run_all_txt.py
@@ -10,6 +10,7 @@ for filename in os.listdir(directory):
         continue
     if 'output' in filename:
         continue
+    print('running {}'.format(filename))
     os.system('python3 maze.py --file {} -print'.format(directory + '/' + filename))
     os.system('python3 maze.py --file {} >{}'.format(directory + '/' + filename, directory + '/' + filename.split('.')[0]+'_my_output.txt'))
     os.system('diff {} {}'.format(directory + '/' + filename.split('.')[0]+'_output.txt', directory + '/' + filename.split('.')[0]+'_my_output.txt'))
